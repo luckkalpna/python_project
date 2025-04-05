6604e03d206ce6f6da093d084bd54df87ccb9c7d
luckkalpna <luckkalpna@gmail.com>
2025-04-05 17:16:51 +0530

    fetch files from static & templates folders

--- Hello/Hello/settings.py
+++ Hello/Hello/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Hello.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,9 +114,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Added Manually
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
--- Hello/home/views.py
+++ Hello/home/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse("this is home page")
+  return render(request, 'index.html')
+  # return HttpResponse("this is home page")
 
 def about(request):
   return HttpResponse("this is about page")
